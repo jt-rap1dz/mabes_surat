a3d3e2402619b42a78de6598a5d036d781bad881
Agung Jati <jt.sascyber16@gmail.com>
2024-07-10 10:08:11 +0700

    10072024

--- app/Http/Controllers/KesatuanController.php
+++ app/Http/Controllers/KesatuanController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Kesatuan; // ini memanggil file model kesatuan
-use DB; // tipe kodingan untuk memkasi query builder
+use DB; // tipe kodingan untuk memakai library database query builder
 // atau menggunakan
 //use illuminate\Support\Facades\DB;
 
@@ -18,7 +18,10 @@ class KesatuanController extends Controller
         $kesatuan = DB::table('kesatuan')->get();
         //variabel keastuan menyimpan data table kesatuang yang diambil
         //dari model kesatuan.php
-        return view('admin.kesatuan.index');
+        return view('admin.kesatuan.index', compact ('kesatuan')); //compact untuk menampilkan data table
+        //return view mengarahakan ke folder admin/kesatuan/index.blade.php
+        //sekaligus mengirimkan variable $kesatuan
+        //melalui compact 
     }
 
     /**
